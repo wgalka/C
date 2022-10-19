4520a937567134f3e563be55b6e01a89351785c4
wgalka <wgalka2@op.pl>
2022-10-19 10:59:34 +0200

    init

--- source/lab3/loop.c
+++ source/lab3/loop.c
@@ -2,16 +2,25 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("petla for");
-    for (int i = 20; i < 0; i-- ){
-        printf("Wartosc %i\n",i);
+    printf("petla for\n");
+    for (int i = 20; i > 0; i--)
+    {
+        if (i == 5)
+        {
+            // break;
+            continue; // przeszkoczy do kolejnej iteracji
+            // czyli wartość 5 zostanie pominięta
+        }
+
+        printf("Wartosc %i\n", i);
     }
     int i = 0;
-    
+
     printf("petla while");
 
-    while(i<20){
-        printf("Wartosc %i\n",i);
+    while (i < 20)
+    {
+        printf("Wartosc %i\n", i);
         i++;
     }
     /* code */
@@ -19,9 +28,10 @@ int main(int argc, char const *argv[])
     printf("petla do-while");
 
     i = 0;
-    do{
-        printf("Wartosc %i\n",i);
+    do
+    {
+        printf("Wartosc %i\n", i);
         i++;
-    } while(i<20);
+    } while (i < 20);
     return 0;
 }
---
+++ source/lab4/pointers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int *wsk; // deklaracja zmiennej przechowującej adres w pamięci. (wskaźnik)
+
+    char a = 'a';
+
+    printf("Wartosc przechowywana przez zmienna a: %c\n", a);
+
+    // Za pomocą operatora &nazwa_zmiennej można odczytać adres komórki w pamięci.
+    // !Adres jest w formacie szesnastkowym, należy użyć odpowiedniego typu funkcji printf() aby go wypisać poprawnie.
+    printf("Wartosc zmiennej a znajduje sie pod adresem: %p\n", &a);
+
+    wsk = &a; // Przypisanie adresu zmiennej a dla zmiennej wskaźnikowej
+    
+    printf("Wartosc zmiennej a znajduje sie pod adresem: %p \n", wsk);
+    // Aby odczytać wartość znajdującą się pod danym adresem używamy operatora wyłuskania "*"
+    printf("Wartosc przechowywana pod adresem przechowaynym przez wskaznik %c", *wsk);
+
+    
+
+    return 0;
+}
