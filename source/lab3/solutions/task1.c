ea9047f2ce3023aa347dd3ea7db8581365e1c0a0
wgalka <wgalka2@op.pl>
2022-10-26 08:24:59 +0200

    init

---
+++ source/lab3/solutions/task1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float tab[4][2];
+
+    tab[0][0] = 0.0;
+    tab[0][1] = 0.0;
+    tab[1][0] = 0.0;
+    tab[1][1] = 0.0;
+    tab[2][0] = 0.0;
+    tab[2][1] = 0.0;
+
+    
+
+    return 0;
+}
