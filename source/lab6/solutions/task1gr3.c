72c0e9892ce44baeaf9bfd3723edbdd008353d28
wgalka <wgalka2@op.pl>
2022-11-16 13:47:41 +0100

    init

--- source/lab6/solutions/task1gr3.c
+++ source/lab6/solutions/task1gr3.c
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
     {
         for (int b = 0; b <= 1; b++)
         {
-            printf("%i |%i |%i  |%i  |%i  |%i  |\n",a,b, !a, !b, a&&b, a||b);
+            printf("|%i |%i |%i  |%i  |%i  |%i  |\n",a,b, !a, !b, a&&b, a||b);
         }
     }
 
