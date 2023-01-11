aeeb7bd907cb308b9c7e84481f0c87a292e929b0
kompotmalinowy <wgalka2@op.pl>
2023-01-11 14:32:14 +0100

    init

--- TEMP/11012023.c
+++ TEMP/11012023.c
@@ -7,13 +7,13 @@ typedef struct tablica{
     float **tab;
 } tablica;
 
-void matrix_multiplication(float **tab, unsigned int col, int row, float x)
+void matrix_multiplication(tablica tab, float x)
 {
-    for (int i = 0; i < row; i++)
+    for (int i = 0; i < tab.row; i++)
     {
-        for (int j = 0; j < col; j++)
+        for (int j = 0; j < tab.col; j++)
         {
-           tab[i][j] *= x; // tab[i] = tab[i] * x
+           tab.tab[i][j] *= x; // tab[i] = tab[i] * x
         }
     }
 }
@@ -42,36 +42,24 @@ tablica createMatrix(int row, int col){
         tab3[i] = calloc(2, sizeof(float));
     }
     x.tab = tab3;
+    return x;
 }
 
 int main(int argc, char const *argv[])
 {
-    tablica asd;
-    
-    int row = 4;
-    int col = 2;
-    float tab[4][2] = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
-    float tab2[4][2] = {1, 2, 2, 3, 3, 4, 4, 5};
-    // matrix_multiplication(tab, 2, 4, 4);
+    tablica asd = createMatrix(4,2);
 
-    float **tab3;
-    tab3 = calloc(4, sizeof(float *));
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < asd.row; i++)
     {
-        tab3[i] = calloc(2, sizeof(float));
-    }
-
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
+        for (int j = 0; j < asd.col; j++)
         {
             printf("podaj wartosc tab[%d][%d]", i, j);
             float val;
             scanf("%f", &val);
-            tab3[i][j] = val;
+            asd.tab[i][j] = val;
         }
     }
 
-    print_matrix(tab3, 4, 2);
+    print_matrix(asd);
     return 0;
 }
