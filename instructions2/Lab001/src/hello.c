c4e8f367e93d4b49dc74db2473ab47dd016d72b4
kompotmalinowy <wgalka2@op.pl>
2023-10-15 20:10:12 +0200

    init

--- instructions2/Lab001/src/hello.c
+++ instructions2/Lab001/src/hello.c
@@ -1,9 +1,12 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello, World!\n");
+    printf("Hello World! ");
+    system("PAUSE");
+    printf("\b\b\bWojtek");
     system("PAUSE");
     return EXIT_SUCCESS;
 }
